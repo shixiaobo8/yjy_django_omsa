0515ac1b94dcc06e18317ff3c7648d8b8d395194
shixiaobo8 <shixiaobo8@163.com>
2023-07-17 11:45:50 +0800

    Update ali_client.py

--- Myapp/aliyun/ali_client.py
+++ Myapp/aliyun/ali_client.py
@@ -9,7 +9,7 @@ from aliyunsdkcore import client
 class ali_client(object):    
       
     def __init__(self):
-        self.config = ('LTAIYj7b9Fm1rrH2','6rWkgQX8yiIDrOY70vcy19EUuHvjW2','cn-beijing')
+        self.config = ('xxxxxxxx','xxxxxxxxxxxxxxxxxx','cn-shenxi')
         self.alt = client.AcsClient(self.config[0],self.config[1],self.config[2])
         
     def getClient(self):
